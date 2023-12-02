3ee2f99f401afeb92f98163e0e6522752f101ef1
DBeast <support@dbeast.co>
2023-12-02 14:12:49 +0200

    [APP] Update dashboards

--- dbeast-monitoring-for-elastic-stack-app/.config/webpack/constants.ts
+++ dbeast-monitoring-for-elastic-stack-app/.config/webpack/constants.ts
@@ -1,3 +1,4 @@
 export const SOURCE_DIR = 'src';
-export const DIST_DIR = 'dest';
-// export const DIST_DIR = '/Users/yevgenylevin/Downloads/grafana-9.4.7/data/plugins/dbeast-toolkit-app';
+export const DIST_DIR = 'dist';
+
+
--- dbeast-monitoring-for-elastic-stack-app/CHANGELOG.md
+++ dbeast-monitoring-for-elastic-stack-app/CHANGELOG.md
@@ -1,5 +1,7 @@
 # Changelog
+## 0.5.0
 
-## 1.0.0 (Unreleased)
+
+## 0.4.0
 
 Initial release.
