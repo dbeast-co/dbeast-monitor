56df894ae2312ef4d1c620aa8702121e70670452
Vakhtang <tankha@gmail.com>
2025-11-27 15:58:38 +0200

    - Fix docker compose

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,11 +8,12 @@ fi
 echo "Starting development mode"
 
 if grep -i -q alpine /etc/issue; then
+    echo "Starting alpine supervisord"
     exec /usr/bin/supervisord -c /etc/supervisord.conf
 elif grep -i -q ubuntu /etc/issue; then
+    echo "Starting ubuntu supervisord"
     exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 else
     echo 'ERROR: Unsupported base image'
     exit 1
 fi
-
